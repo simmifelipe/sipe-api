8388812312fb442cc37d9df1791b5b0294f83125
Felipe Simmi <simmifelipe1992@gmail.com>
2018-10-18 20:00:55 -0300

    Removidas importações.

--- src/main/java/com/g3softwares/sipe/api/model/Empresa.java
+++ src/main/java/com/g3softwares/sipe/api/model/Empresa.java
@@ -1,18 +1,14 @@
 package com.g3softwares.sipe.api.model;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
